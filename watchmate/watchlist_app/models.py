b48a925bd3823dad0b99addcd6e8a7391fb6b0bc
Fernandez Francisco <Franfer0301@gmail.com>
2024-11-16 14:12:51 -0300

    ..

--- watchmate/watchlist_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Movie(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.title
